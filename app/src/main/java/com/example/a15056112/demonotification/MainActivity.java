6d3f0c878e19d85d58605ae48c000ac09322d7d0
15056112 <15056112@myrp.edu.sg>
2017-05-25 19:57:08 +0800

    Notify 2 completed

--- app/src/main/java/com/example/a15056112/demonotification/MainActivity.java
+++ app/src/main/java/com/example/a15056112/demonotification/MainActivity.java
@@ -56,5 +56,44 @@ public class MainActivity extends AppCompatActivity {
                  finish();
              }
          });
+
+        btnNotify2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+
+                PendingIntent pIntent = PendingIntent.getActivity
+                        (MainActivity.this, requestCode,
+                                intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                Notification.BigTextStyle bigText = new
+                        Notification.BigTextStyle();
+                bigText.bigText("This is one big text - A quick brown fox jumps over a lazy brown dog");
+                bigText.setBigContentTitle("Big Text – Long Content");
+                bigText.setSummaryText("Reflection Journal?");
+
+                // Build notification
+                Notification.Builder builder = new
+                        Notification.Builder(MainActivity.this);
+                builder.setContentTitle("Amazing Offer!");
+                builder.setContentText("Subject");
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+                builder.setContentIntent(pIntent);
+                builder.setStyle(bigText);
+
+                builder.setAutoCancel(true);
+
+                Notification n = builder.build();
+
+                NotificationManager notificationManager = (NotificationManager)
+                        getSystemService(NOTIFICATION_SERVICE);
+
+                // This replaces the existing notification with the same ID
+                notificationManager.notify(notificationID, n);
+                finish();
+
+
+            }
+        });
     }
 }
